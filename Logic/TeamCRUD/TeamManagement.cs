4e685cbcb59a9728e58ca0bc9eff5039698ccbe3
Ramos <aara@itu.dk>
2015-11-12 16:49:43 +0100

    teamManagement

--- Logic/TeamCRUD/TeamManagement.cs
+++ Logic/TeamCRUD/TeamManagement.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logic.StorageManagement;
 
 namespace Logic.TeamCRUD
 {
     class TeamManagement
     {
+        TeamSaver _teamSaver;
+
         public TeamManagement()
         {
-
+            _teamSaver = new TeamSaver();
         }
+
+        public TeamManagement(TeamSaver teamSaver)
+        {
+            _teamSaver = teamSaver;
+        }
+
+        public void CreateTeam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUserToTeam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUserFromTeam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTeam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RetrieveTeam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTeam()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
+
