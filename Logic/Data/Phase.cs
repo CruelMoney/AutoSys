9152fcb3d64eabacbde1a7418934b0b2a2e44dea
Emil Hørning <emil_chr@msn.com>
2015-11-12 18:15:28 +0100

    Mere ændring til data

---
+++ Logic/Data/Phase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Data
+{
+    class Phase
+    {
+        public List<Task> TasksToDo { get; set; }
+        public List<Task> Finishedtask { get; set; }
+        public Boolean isFinished { get; set; }
+        public enum PhaseType
+        {
+            ReviewPhase,
+            ValidatePhase
+         }
+
+
+     }
+}
--- Logic/Data/Task.cs
+++ Logic/Data/Task.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Storage.Repository;
 
 namespace Logic.Data
 {
-    class Task
+    class Task : IEntity
     {
+        public int Id { get; set; }
+        public User AssociatedUser { get; set; }
+        public Boolean TaskDone { get; set; }
+
     }
 }
