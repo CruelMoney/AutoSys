2a5b20cad027e88b9efa7850431212c902729ff2
Gonaden <kathrinkat@hotmail.com>
2015-11-12 16:27:51 +0100

    Added flatfile- and entityframeworkrepository

---
+++ Storage/Repository/EntityFrameworkRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public class EntityFrameworkRepository<T> : IRepository<T> where T : class, IEntity
+    {
+        public EntityFrameworkRepository()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Storage/Repository/FlatFileRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public class FlatFileInventory<T> : IRepository<T> where T : IEntity
+    {
+
+        public FlatFileInventory(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
