392a60a95b6a6756bc9d25e1e3e368309e6c1965
cruelmoney <chrdengso@gmail.com>
2015-11-13 14:25:19 +0100

    hey

---
+++ Logic/Data/TeamDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Data.StoredData;
+using Storage.Repository;
+
+namespace Logic.Data
+{
+    public class TeamDataContext:DbContext,IDbContext
+    {
+        public DbSet<StoredTeam> Teams { get; set; }
+        public DbSet<StoredUser> Users { get; set; }
+  
+    }
+}
