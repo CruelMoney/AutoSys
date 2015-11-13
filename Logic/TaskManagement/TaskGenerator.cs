8606bf61878f1f07a29310b33aa6c1f3bbcc5745
cruelmoney <chrdengso@gmail.com>
2015-11-13 11:59:29 +0100

    Fixed storage, added task generator

--- Logic/TaskManagement/TaskGenerator.cs
+++ Logic/TaskManagement/TaskGenerator.cs
@@ -4,6 +4,8 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logic.Data;
+using Storage;
 using Storage.Repository;
 
 namespace Logic.TaskManagement
@@ -11,9 +13,14 @@ namespace Logic.TaskManagement
     public class TaskGenerator
     {
         private readonly IRepository _repository;
-        public TaskGenerator(IRepository repo, )
+        private readonly User _user;
+        private readonly Study _study;
+
+        public TaskGenerator(IRepository repo, Study study, User user)
         {
             _repository = repo;
+            _study = study;
+            _user = user;
         }
     }
 }
