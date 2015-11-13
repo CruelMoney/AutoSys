441ea942843167c49bf2ddfbfe0325b932ae883b
cruelmoney <chrdengso@gmail.com>
2015-11-13 13:30:54 +0100

    StoredClasses

---
+++ Logic/Data/StoredData/StoredItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Storage.Repository;
+
+namespace Logic.Data
+{
+    /// <summary>
+    ///     This class defines a bibliographic item.
+    /// </summary>
+    public class StoredItem:IEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Logic/Data/StoredData/StoredStudy.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using Storage.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace Logic.Data
+{
+    public class StoredStudy : IEntity
+    {
+        public int Id { get; set; }
+    }
+
+}
