0962e7650762234effd8c3be90a4e9eb88390875
Gonaden <kathrinkat@hotmail.com>
2015-11-26 16:19:07 +0100

    added ConflictingTasks to StageLogic

--- Logic/Model/StageLogic.cs
+++ Logic/Model/StageLogic.cs
@@ -14,6 +14,7 @@ namespace Logic.Model
         public virtual List<CriteriaLogic> Criteria { get; set; } // reference til Criteria (one to many)
         public virtual List<TaskLogic> Tasks { get; set; } // reference til Task (one to many)
         public virtual StudyLogic Study { get; set; } // reference til Study (many to one)
+        public List<TaskLogic> ConflictingTasks { get; set; }
 
     }
 }
