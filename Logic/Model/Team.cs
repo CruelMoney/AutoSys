13f7df40b9cc101c049aa9f54c138887cbe09fe1
cruelmoney <chrdengso@gmail.com>
2015-11-26 14:39:24 +0100

    moved filed

--- Logic/Model/Team.cs
+++ Logic/Model/Team.cs
@@ -10,7 +10,7 @@ namespace Logic.Model
     public class Team : IEntity
     {
         public int Id { get; set; }
-        public string Name { get;  set}
+        public string Name { get; set; }
         public int UserId { get; set; }
         public virtual List<User> Users { get; set; }
     }
