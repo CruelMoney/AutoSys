178ba1f4781023f1c691a6b49ae56e284712b93f
Gonaden <kathrinkat@hotmail.com>
2015-11-26 14:07:49 +0100

    New storage system

--- Logic/Model/Data/StoredStudy.cs
+++ Logic/Model/Data/StoredStudy.cs
@@ -7,9 +7,5 @@ namespace Logic.Model.Data
     public class StoredStudy : IEntity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int teamId { get; set; }
-        public Team ascociatedTeam { get; set; }
-        public List<Item> studyData { get; set; }
     }
 }
