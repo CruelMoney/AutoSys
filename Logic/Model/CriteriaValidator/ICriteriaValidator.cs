c6bc40d3ca7db7ac1280c2201bf8ba207f6c4475
cruelmoney <chrdengso@gmail.com>
2015-12-01 14:05:07 +0100

    added tests for entity framework implementation, added model for allowing one task to have multiple data entered by different users.

--- Logic/Model/CriteriaValidator/ICriteriaValidator.cs
+++ Logic/Model/CriteriaValidator/ICriteriaValidator.cs
@@ -6,6 +6,7 @@
         /// Checks whether a specified criteria is met.
         /// </summary>
         /// <param name="criteria">The item to Validate.</param>
+        /// <param name="data"></param>
         /// <returns>true when the specified item is valid; false otherwise.</returns>
         bool CriteriaIsMet(Criteria criteria, DataFieldLogic data);
     }
