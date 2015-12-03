ac436a4a43d8cc79f85eba10afb8c328fe205205
Emil Hørning <emil_chr@msn.com>
2015-12-03 10:43:28 +0100

    Added webapi project

---
+++ StudyConfigurationServer/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace StudyConfigurationServer.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
