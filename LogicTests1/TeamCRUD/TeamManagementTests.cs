1fe69f4d41669a51b6aea59f2d7f47406f49e076
Gonaden <kathrinkat@hotmail.com>
2015-12-07 11:43:26 +0100

    Team testing

--- LogicTests1/TeamCRUD/TeamManagementTests.cs
+++ LogicTests1/TeamCRUD/TeamManagementTests.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyConfigurationServer.Models;
 
 namespace LogicTests1.TeamCRUD
 {
     [TestClass()]
     public class TeamManagementTests
     {
+        Team testTeam;
+        User testUser1;
+        User testUser2;
+        List<User> _users = new List<User>();
+
+        [TestInitialize]
+        public void InitializeTeamTests() {
+            testUser1 = new User() { Id = 1, Name = "testUser1" };
+            testUser2 = new User() { Id = 2, Name = "testUser2" };
+            _users.Add(testUser1);
+            testTeam = new Team() { Id = 1, Name = "Test Team", Users = _users };
+        }
+
         [TestMethod()]
         public void TeamManagementTest()
         {
@@ -15,19 +30,21 @@ namespace LogicTests1.TeamCRUD
         [TestMethod()]
         public void TeamCreateTest()
         {
-            throw new NotImplementedException();
+            Assert.AreEqual("Test Team", testTeam.Name);
         }
 
         [TestMethod()]
         public void TeamAddUserTest()
         {
-            throw new NotImplementedException();
+            testTeam.Users.Add(testUser2);
+            Assert.AreEqual(2, testTeam.Users.Count);
         }
 
         [TestMethod()]
         public void TeamRemoveUserTest()
         {
-            throw new NotImplementedException();
+            testTeam.Users.Remove(testUser1);
+            Assert.AreEqual(0, testTeam.Users.Count);
         }
 
         [TestMethod()]
