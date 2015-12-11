d3b2d4bcd15e29d2f1e5f1711e3bad448ada1514
cruelmoney <chrdengso@gmail.com>
2015-12-11 13:32:59 +0100

    Updated DTO's

--- StudyConfigurationServer/Models/DTO/StageDTO.cs
+++ StudyConfigurationServer/Models/DTO/StageDTO.cs
@@ -12,15 +12,60 @@ namespace StudyConfigurationServer.Models.DTO
         public int Id { get; set; }
         //The criteria are defining what fields are going to be editable for this stage
         [Required]
-        public Criteria[] Criteria { get; set; }
+        public CriteriaDTO[] Criteria { get; set; }
         public int StudyID { get; set; }
-        //The fields that can only be seen in adddition to the editable fields.
-        public Item.FieldType[] VisibleFields { get; set; }
         [Required]
         public int[] ReviewerIDs { get; set; }
         public int[] ValidatorIDs { get; set; }
         [Required]
-        public Stage.Distribution DistributionRule { get; set; }
-        
+        public Distribution DistributionRule { get; set; }
+        //The fields that can only be seen in adddition to the editable fields.
+        public FieldType[] VisibleFields { get; set; }
+
+        public enum Distribution
+        {
+            FiftyPercentOverlap,
+            HundredPercentOverlap,
+            NoOverlap,
+        }
+
+        public enum FieldType
+        {
+            Address,
+            Annote,
+            Author,
+            Booktitle,
+            Chapter,
+            Crossref,
+            Edition,
+            Editor,
+            HowPublished,
+            Instritution,
+            Journal,
+            Key,
+            Month,
+            Note,
+            Number,
+            Organization,
+            Pages,
+            Publisher,
+            School,
+            Series,
+            Title,
+            Type,
+            Volume,
+            Year,
+            URL,
+            ISBN,
+            ISSN,
+            LCCN,
+            Abstract,
+            Keywords,
+            Price,
+            Copyright,
+            Language,
+            Contents,
+            Doi,
+        }
     }
 }
